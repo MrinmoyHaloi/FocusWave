6bad02b42468b8af5f6e562e23b53c39e9d93d11
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-06-12 20:14:55 +0530

    fix: remove backdrop blur filter due to performance reasons

--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -203,8 +203,7 @@
 		text-align: center;
 		border-radius: 10rem;
 		border: rgba(44, 44, 44, 0.8) solid 3px;
-		background: radial-gradient(at 50% -10%, rgba(63, 63, 63, 0.8) 0%, rgb(17, 17, 17, 0.82) 100%);
-		backdrop-filter: blur(10px);
+		background: radial-gradient(at 50% -10%, rgba(63, 63, 63, 0.9) 0%, rgb(17, 17, 17, 0.95) 100%);
 		.container {
 			display: flex;
 			flex-direction: column;
