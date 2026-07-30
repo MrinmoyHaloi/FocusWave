650c85489b7e3ed797db7c541f42c3099cf11e6c
MrinmoyHaloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2026-07-30 12:41:57 +0530

    fix: new and enhanced set time and times up dialog

--- src/lib/SetTimeDialog.svelte
+++ src/lib/SetTimeDialog.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Icon from '@iconify/svelte';
-
 	let { handleSetTimeSubmit, tempMin = $bindable(), tempSec = $bindable() } = $props();
 	let dialog;
 
@@ -25,28 +24,129 @@
 			dialog.close();
 		});
 	}
+
+	$effect(() => {
+		dialog.addEventListener('click', (e) => {
+			const dialogDimensions = dialog.getBoundingClientRect();
+			if (
+				e.clientX < dialogDimensions.left ||
+				e.clientX > dialogDimensions.right ||
+				e.clientY < dialogDimensions.top ||
+				e.clientY > dialogDimensions.bottom
+			) {
+				if (!document.startViewTransition) {
+					dialog.close();
+					return;
+				}
+
+				document.startViewTransition(() => {
+					dialog.close();
+				});
+			}
+		});
+	});
+	let min = $state(tempMin);
+	let sec = $state(tempSec);
+
+	$effect(() => {
+		tempMin = min;
+		tempSec = sec;
+	});
 </script>
 
 <dialog bind:this={dialog} style="view-transition-name: dialog;">
 	<div class="container">
 		<div class="flex justify-between">
-			<h2 class="flex-1 text-center text-4xl font-semibold">Set time</h2>
+			<h2
+				class="flex-1 text-center text-2xl font-medium"
+				style="text-box: trim-both cap alphabetic;">
+				Set time
+			</h2>
 			<button class="hover:opacity-75" onclick={close}>
 				<Icon icon="mdi:close" width="1.25rem" height="1.25rem" />
 			</button>
 		</div>
+		<div class="-mx-6 mb-5 mt-6 h-[3px]" style="background-color: rgba(44, 44, 44, 0.8);"></div>
 		<form onsubmit={handleSetTimeSubmit}>
-			<div class="flex gap-3 *:flex-1">
-				<div class="grid text-center text-2xl font-bold text-gray-400">
-					<span>min</span>
-					<input type="number" name="min" id="min" min="0" max="60" required bind:value={tempMin} />
+			<div class="mb-2 flex text-2xl text-[#969696] *:flex-1">
+				<span>mm</span>
+				<span>ss</span>
+			</div>
+
+			<div
+				class="flex text-2xl *:flex *:flex-1 *:justify-center *:*:rounded-lg *:*:bg-[#383839] *:*:text-[#b6b6b6]">
+				<div>
+					<button
+						onclick={(e) => {
+							e.preventDefault();
+							min = Math.min((parseInt(min) || 0) + 1, 60);
+						}}>
+						<Icon icon="lucide:chevron-up" width="2rem" height="2rem" />
+					</button>
 				</div>
-				<div class="text-center text-2xl font-bold text-gray-400">
-					<span>sec</span>
-					<input type="number" name="sec" id="sec" min="0" max="59" required bind:value={tempSec} />
+				<div>
+					<button
+						onclick={(e) => {
+							e.preventDefault();
+							sec = Math.min((parseInt(sec) || 0) + 1, 59);
+						}}>
+						<Icon icon="lucide:chevron-up" width="2rem" height="2rem" />
+					</button>
 				</div>
 			</div>
-			<div>
+
+			<div
+				class="my-5 flex overflow-hidden rounded-[50px]"
+				style="border: #484848FF solid 3px;gap:3px;background-color:#484848FF;">
+				<input
+					type="text"
+					inputmode="numeric"
+					class="w-full text-center text-8xl font-medium"
+					name="min"
+					id="min"
+					min="0"
+					max="60"
+					pattern="[0-9]*"
+					title="Numeric values"
+					required
+					bind:value={min} />
+
+				<input
+					type="text"
+					inputmode="numeric"
+					class="w-full text-center text-8xl font-medium"
+					name="sec"
+					id="sec"
+					min="0"
+					max="59"
+					pattern="[0-9]*"
+					title="Numeric values"
+					required
+					bind:value={sec} />
+			</div>
+			<div
+				class="flex text-2xl *:flex *:flex-1 *:justify-center *:*:rounded-lg *:*:bg-[#383839] *:*:text-[#B6b6b6]">
+				<div>
+					<button
+						onclick={(e) => {
+							e.preventDefault();
+							min = Math.max((parseInt(min) || 0) - 1, 0);
+						}}>
+						<Icon icon="lucide:chevron-down" width="2rem" height="2rem" />
+					</button>
+				</div>
+				<div>
+					<button
+						onclick={(e) => {
+							e.preventDefault();
+							sec = Math.max((parseInt(sec) || 0) - 1, 0);
+						}}>
+						<Icon icon="lucide:chevron-down" width="2rem" height="2rem" />
+					</button>
+				</div>
+			</div>
+
+			<div class="flex justify-end">
 				<button
 					type="submit"
 					class="start-btn mt-4 rounded-full border border-slate-700 px-4 py-2 text-white">
@@ -59,25 +159,15 @@
 
 <style lang="scss">
 	dialog {
-		max-width: 27rem;
-		padding: 2rem;
+		max-width: 30rem;
+		padding: 1.5rem;
 		border-radius: 1rem;
 		box-shadow: 0 0 40px #18d9fb29;
 		border: rgba(44, 44, 44, 0.8) solid 3px;
-		background: radial-gradient(at 50% -10%, rgba(63, 63, 63, 1) 0%, rgb(17, 17, 17, 0.95) 100%);
-		.container {
-			display: flex;
-			flex-direction: column;
-			gap: 1rem;
-		}
+		background: rgb(27, 27, 27);
 		&::backdrop {
 			background: rgba(0, 0, 0, 0.5);
-		}
-		#min,
-		#sec {
-			padding: 0.2em 0.5em;
-			border: 2px solid #2242d2a7;
-			border-radius: 4rem;
+			backdrop-filter: blur(10px);
 		}
 	}
 </style>
--- src/lib/TimesUpDialog.svelte
+++ src/lib/TimesUpDialog.svelte
@@ -23,26 +23,50 @@
 			dialog.close();
 		});
 	}
+	
+	$effect(() => {
+		dialog.addEventListener('click', (e) => {
+			const dialogDimensions = dialog.getBoundingClientRect();
+			if (
+				e.clientX < dialogDimensions.left ||
+				e.clientX > dialogDimensions.right ||
+				e.clientY < dialogDimensions.top ||
+				e.clientY > dialogDimensions.bottom
+			) {
+				if (!document.startViewTransition) {
+					dialog.close();
+					return;
+				}
+
+				document.startViewTransition(() => {
+					dialog.close();
+				});
+			}
+		});
+	});
 </script>
 
 <dialog bind:this={dialog} style="view-transition-name: dialog;">
-	<div class="container">
-		<h2 class="text-4xl font-bold">Time's up!</h2>
-		<p class="text-lg text-gray-300">
-			You have completed your focus session. Take a break and relax for a while or start again.
-		</p>
-		<div>
-			<button class="break-btn mt-4 rounded-full px-4 py-2 text-white" onclick={close}>
-				Take a Break
-			</button>
-			<button
-				class="start-btn mt-4 rounded-full border border-slate-700 px-4 py-2 text-white"
-				onclick={() => {
-					toggleTimer();
-					close();
-				}}>
-				Start Again
-			</button>
+	<div class="flex flex-col">
+		<h2 class="text-2xl font-medium">Time's up!</h2>
+		<div class="-mx-6 mb-5 mt-6 h-[3px]" style="background-color: rgba(44, 44, 44, 0.8);"></div>
+		<div class="flex flex-col">
+			<p class="text-lg text-gray-300">
+				You have completed your focus session. Take a break and relax for a while or start again.
+			</p>
+			<div class="flex gap-4 btns mt-2">
+				<button class="break-btn mt-4 rounded-full px-4 py-2 text-white" onclick={close}>
+					Take a Break
+				</button>
+				<button
+					class="start-btn mt-4 rounded-full border border-slate-700 px-4 py-2 text-white"
+					onclick={() => {
+						toggleTimer();
+						close();
+					}}>
+					Start Again
+				</button>
+			</div>
 		</div>
 	</div>
 </dialog>
@@ -50,38 +74,37 @@
 <style lang="scss">
 	dialog {
 		max-width: 35rem;
-		padding: 3rem;
+		padding: 1.5rem;
 		box-shadow: 0 0 40px #18d9fb29;
 		text-align: center;
-		border-radius: 10rem;
+		border-radius: 1rem;
 		border: rgba(44, 44, 44, 0.8) solid 3px;
-		background: radial-gradient(at 50% -10%, rgba(63, 63, 63, 1) 0%, rgb(17, 17, 17, 0.95) 100%);
-		.container {
-			display: flex;
-			flex-direction: column;
-			gap: 1rem;
-		}
+		background: rgb(27, 27, 27);
+
 		&::backdrop {
 			background: rgba(0, 0, 0, 0.5);
+			backdrop-filter: blur(10px);
 		}
 		div {
 			display: flex;
 			justify-content: center;
-			gap: 1rem;
-			.break-btn {
-				background: linear-gradient(to bottom, #00e0ff 0%, #0001ff 100%);
-				box-shadow: 0 0 10px rgba(0, 224, 255, 0.5) inset;
-				&:hover {
-					background: linear-gradient(to bottom, #00e0ff 0%, #063cff 100%);
-					box-shadow: 0 0 10px rgba(0, 224, 255, 0.5);
+			.btns {
+
+				.break-btn {
+					background: linear-gradient(to bottom, #00e0ff 0%, #0001ff 100%);
+					box-shadow: 0 0 10px rgba(0, 224, 255, 0.5) inset;
+					&:hover {
+						background: linear-gradient(to bottom, #00e0ff 0%, #063cff 100%);
+						box-shadow: 0 0 10px rgba(0, 224, 255, 0.5);
+					}
 				}
-			}
-			.start-btn {
-				background: transparent;
-				border: 1px solid #ffffff33;
-				&:hover {
-					background: rgba(255, 255, 255, 0.1);
-					box-shadow: 0 0 10px rgba(255, 255, 255, 0.2);
+				.start-btn {
+					background: transparent;
+					border: 1px solid #ffffff33;
+					&:hover {
+						background: rgba(255, 255, 255, 0.1);
+						box-shadow: 0 0 10px rgba(255, 255, 255, 0.2);
+					}
 				}
 			}
 		}
